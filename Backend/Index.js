9f997af9ad71bd47e4c5d7e36640f5c29ef75722
Himanshu Gurjar <himanshugurjarbhai@gmail.com>
2025-11-16 17:49:07 +0530

    change render link

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,38 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import mongoose from "mongoose";
-// import router from "./Router/UserRouter.js";
-// import postrouter from "./Router/PostRouter.js";
-
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   })
-// );
-
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("✅ Database Connected Successfully"))
-//   .catch((err) => console.log("❌ Database Connection Failed:", err));
-
-// app.use("/api", router);
-// app.use("/post", postrouter);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server is running on http://localhost:${PORT}`);
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -48,7 +13,8 @@ app.use(express.json());
 // ✅ Allow both local & deployed frontend dynamically
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://hubly-t2vc.onrender.com",
+  // "https://hubly-t2vc.onrender.com",
+  "https://hubly-io-a026.onrender.com"
 ];
 
 app.use(
